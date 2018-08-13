fc32e5532342358357265e51cd0c8b96fd32f0ca
Maria Michailidou <michaelidoumary@gmail.com>
2018-08-14 00:23:46 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,81 +1,53 @@
+//Importing the dependencies and the components
 import React from "react";
 import * as BooksAPI from "./BooksAPI";
 import "./App.css";
-import Search from "./Search.js";
-import Shelf from "./Shelf.js";
-import { Link, Route } from "react-router-dom";
+import HomePage from "./HomePage.js";
+import SearchPage from "./SearchPage.js";
+import { Route } from "react-router-dom";
 
 class App extends React.Component {
-  state = {
-    books: []
-  };
-  componentDidMount() {
-    BooksAPI.getAll().catch(error =>
-      console.error("Failed to fetch books", error)
-    );
-    BooksAPI.getAll().then(books => {
-      this.setState({ books });
-      console.log("ReadsApp mounted");
-    });
+  constructor() {
+    super();
+    this.state = {
+      books: []
+    };
   }
-
-  updateBook = (Shelf, Book) => {
-    let result = [];
-    BooksAPI.update(Shelf, Book).then(() => {
-      Book.shelf = Shelf;
-      result = this.state.books.filter(filtered => filtered.id !== Book.id);
-      this.setState({ books: result.concat(Book) });
+  //Message that will be showed when the app.js runs correctly
+  componentDidMount() {
+    this.setBooksState();
+    console.log("App mounted");
+  }
+  //Method that updates the shelf when we move the book
+  ShelfUpdate = (book, shelf) => {
+    BooksAPI.update(book, shelf).then(() => {
+      this.setBooksState();
     });
   };
-
+  //Method that prints the books and sets their state
+  setBooksState() {
+    BooksAPI.getAll().then(books => this.setState({ books }));
+  }
+  //render function
   render() {
+    //return method
     return (
+      //HomePage component
       <div className="app">
         <Route
           exact
-          path={`/`}
+          path="/"
           render={() => (
-            <div className="list-books">
-              <div className="list-books-title">
-                <h1>MyReads</h1>
-              </div>
-              <div className="list-books-content">
-                <div>
-                  <h3 className="bookshelf-title">Currently Reading</h3>
-                  <Shelf
-                    title="currentlyReading"
-                    books={this.state.books}
-                    updateBook={this.updateBook}
-                  />
-
-                  <h3 className="bookshelf-title">Want to Read</h3>
-                  <Shelf
-                    title="wantToRead"
-                    books={this.state.books}
-                    updateBook={this.updateBook}
-                  />
-
-                  <h3 className="bookshelf-title">Read</h3>
-                  <Shelf
-                    title="read"
-                    books={this.state.books}
-                    updateBook={this.updateBook}
-                  />
-                </div>
-              </div>
-              <div className="open-search">
-                <Link to={`/search`}>Add a book</Link>
-              </div>
-            </div>
+            <HomePage books={this.state.books} onShelfMove={this.ShelfUpdate} />
           )}
         />
         <Route
+          //SearchPage Component
           path="/search"
           render={() => (
-            <Search
+            <SearchPage
               books={this.state.books}
-              searchQuery={this.searchQuery}
-              updateBook={this.updateBook}
+              onShelfMove={this.ShelfUpdate}
             />
           )}
         />
@@ -83,4 +55,5 @@ class App extends React.Component {
     );
   }
 }
+
 export default App;
